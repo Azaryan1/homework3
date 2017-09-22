ac2e878fe3b93228c0d93b271e8e1205ffb26589
Azaryan1 <h_azaryan@hotmail.com>
2017-09-23 00:11:57 +0400

    first commit

---
+++ homework3.js
@@ -0,0 +1,30 @@
+const first= function(a,b,c) {
+    return (a+b)/c;
+};
+const second= function(){
+    return "Meh";
+};
+const third= function(d,e){
+    console.log(d+e);
+    return "";
+};
+const fullName= function(firstname,lastname){
+    return firstname+" "+lastname;
+};
+const fourth= function(fs,ss,ts){
+if(fs.lenght>ss.lenght && fs.lenght>ts.lenght){return fs;}
+if(ss.lenght>fs.lenght && ss.lenght>ts.lenght){return ss;}
+if(ts.lenght>fs.lenght && ts.lenght>ss.lenght){return ts;}
+};
+const fifth= function(num1,num2){
+    if(num1===num2){return 0;}
+    if(num1>num2){return 1;}
+    if(num2>num1){return -1;}
+};
+const sixth= function(a1,b1,c1){
+    if(!!a1===true){return a1;}
+    if(!!b1===true){return b1;}
+    if(!!c1===true){return c1;}
+    if(!!a1===true && (!!b1===true || !!c1===true)){return a1;}
+    if(!!b1===true && !!c1===true){return b1;}
+};
